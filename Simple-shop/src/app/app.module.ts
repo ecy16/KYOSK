6a2a8f3bb0f695c5ff425c496208d6160f504cb7
ecy16 <ecylamichaels16@gmail.com>
2025-01-30 21:37:25 +0300

    SearchProducts

--- Simple-shop/src/app/app.module.ts
+++ Simple-shop/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { HomeComponent } from './pages/home/home.component';
 import { ProductDetailsComponent } from './pages/product-details/product-details.component';
@@ -12,7 +13,7 @@ import { CheckoutComponent } from './pages/checkout/checkout.component';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, ProductDetailsComponent, CategoriesComponent, CartComponent, CheckoutComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule,FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
